b7833923bb3dca9dd64c95480353f5e62042a963
Vlad <91955559+VladiStas@users.noreply.github.com>
2023-10-20 11:52:14 +0300

    Bind html with django

--- web/webapp/views.py
+++ web/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
@@ -6,3 +6,13 @@ def index(request):
         'products': ['Яблоко', 'Банан', 'Апельсин'],
     }
     return render(request, 'webapp/index.html', context)
+
+def profile(request):
+    return render(request, 'webapp/profile.html')
+
+
+def authorization(request):
+    if request.method == 'POST':
+        return redirect('profile')
+    else:
+        return render(request, 'webapp/authorization.html')
