08437554ee3c2d5dda3e6575b40e524b713d64b0
Vlad <91955559+VladiStas@users.noreply.github.com>
2023-12-14 19:04:51 +0300

    Add comment class for auth

--- web/web/settings.py
+++ web/web/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'appdb',
+        'USER': 'pguser',
+        'PASSWORD': 'pgpassword',
+        'HOST': '192.168.1.161',
+        'PORT': 5432,
     }
 }
 
--- web/webapp/views.py
+++ web/webapp/views.py
@@ -27,6 +27,49 @@ class UserLogoutView(LogoutView):
     next_page = 'projects'
 
 
+# TODO Поменять верхний класс на нижний в релизе
+# class UserLoginView(SuccessMessageMixin, LoginView):
+#     form_class = UserLoginForm
+#     template_name = 'webapp/login.html'
+#     success_message = 'Добро пожаловать на сайт!'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = 'Авторизация на сайте'
+#         return context
+#
+#     def form_valid(self, form):
+#         print("Login successful!")  # Добавьте эту строку
+#         response = super().form_valid(form)
+#         # Подготовка данных для POST-запроса
+#         payload = {
+#             'username': form.cleaned_data['username'],  # Обратите внимание на использование cleaned_data
+#             'password': form.cleaned_data['password'],
+#             # Другие данные, если необходимо
+#         }
+#
+#         # Ваш запрос. Предполагается, что ваш сервер ожидает POST-запрос.
+#         response1 = requests.post('http://localhost:5228/api/Auth/auth', json=payload)
+#
+#         if response1.status_code == 200:
+#             # Если успешно, например, можете сохранить токен в сессии или куках
+#             # и затем перенаправить пользователя
+#             print(response1.json())
+#             return response
+#         else:
+#             # Если запрос неудачен, вы можете обработать ошибку или принять другие меры
+#             print('Authentication failed')
+#             return response
+#
+#     def form_invalid(self, form):
+#         print("Login failed!")  # Добавьте эту строку
+#         return super().form_invalid(form)
+#
+#     def get_success_url(self):
+#         # После успешной авторизации, перенаправляем пользователя на вкладку проектов
+#         return reverse_lazy('user')
+
+
 def main(request):
     return render(request, 'webapp/main.html', {'title': 'Главная'})
 
